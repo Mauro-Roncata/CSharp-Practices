fa15ebb37abfac588d95c37f47d30917f2efd407
Mauro-Roncata <mauroroncata@gmail.com>
2025-10-24 14:27:58 -0300

    stack pratice

--- Colecoes/Form1.Designer.cs
+++ Colecoes/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.btnSortedDict = new System.Windows.Forms.Button();
             this.btnSortedSet = new System.Windows.Forms.Button();
             this.btnQueue = new System.Windows.Forms.Button();
+            this.btnStack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lista
@@ -44,7 +45,7 @@
             this.lista.FormattingEnabled = true;
             this.lista.ItemHeight = 26;
             this.lista.Location = new System.Drawing.Point(9, 10);
-            this.lista.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lista.Margin = new System.Windows.Forms.Padding(2);
             this.lista.Name = "lista";
             this.lista.Size = new System.Drawing.Size(412, 316);
             this.lista.TabIndex = 0;
@@ -52,7 +53,7 @@
             // btnList
             // 
             this.btnList.Location = new System.Drawing.Point(441, 10);
-            this.btnList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnList.Margin = new System.Windows.Forms.Padding(2);
             this.btnList.Name = "btnList";
             this.btnList.Size = new System.Drawing.Size(127, 35);
             this.btnList.TabIndex = 1;
@@ -63,7 +64,7 @@
             // btnHashSet
             // 
             this.btnHashSet.Location = new System.Drawing.Point(441, 50);
-            this.btnHashSet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnHashSet.Margin = new System.Windows.Forms.Padding(2);
             this.btnHashSet.Name = "btnHashSet";
             this.btnHashSet.Size = new System.Drawing.Size(127, 35);
             this.btnHashSet.TabIndex = 2;
@@ -74,7 +75,7 @@
             // btnDict
             // 
             this.btnDict.Location = new System.Drawing.Point(441, 89);
-            this.btnDict.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDict.Margin = new System.Windows.Forms.Padding(2);
             this.btnDict.Name = "btnDict";
             this.btnDict.Size = new System.Drawing.Size(127, 35);
             this.btnDict.TabIndex = 3;
@@ -85,7 +86,7 @@
             // btnSortedList
             // 
             this.btnSortedList.Location = new System.Drawing.Point(441, 129);
-            this.btnSortedList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSortedList.Margin = new System.Windows.Forms.Padding(2);
             this.btnSortedList.Name = "btnSortedList";
             this.btnSortedList.Size = new System.Drawing.Size(127, 35);
             this.btnSortedList.TabIndex = 4;
@@ -96,7 +97,7 @@
             // btnSortedDict
             // 
             this.btnSortedDict.Location = new System.Drawing.Point(441, 169);
-            this.btnSortedDict.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSortedDict.Margin = new System.Windows.Forms.Padding(2);
             this.btnSortedDict.Name = "btnSortedDict";
             this.btnSortedDict.Size = new System.Drawing.Size(127, 35);
             this.btnSortedDict.TabIndex = 5;
@@ -107,7 +108,7 @@
             // btnSortedSet
             // 
             this.btnSortedSet.Location = new System.Drawing.Point(441, 209);
-            this.btnSortedSet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSortedSet.Margin = new System.Windows.Forms.Padding(2);
             this.btnSortedSet.Name = "btnSortedSet";
             this.btnSortedSet.Size = new System.Drawing.Size(127, 35);
             this.btnSortedSet.TabIndex = 6;
@@ -126,11 +127,23 @@
             this.btnQueue.UseVisualStyleBackColor = true;
             this.btnQueue.Click += new System.EventHandler(this.btnQueue_Click);
             // 
+            // btnStack
+            // 
+            this.btnStack.Location = new System.Drawing.Point(441, 291);
+            this.btnStack.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStack.Name = "btnStack";
+            this.btnStack.Size = new System.Drawing.Size(127, 35);
+            this.btnStack.TabIndex = 8;
+            this.btnStack.Text = "Stack";
+            this.btnStack.UseVisualStyleBackColor = true;
+            this.btnStack.Click += new System.EventHandler(this.btnStack_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnStack);
             this.Controls.Add(this.btnQueue);
             this.Controls.Add(this.btnSortedSet);
             this.Controls.Add(this.btnSortedDict);
@@ -139,7 +152,7 @@
             this.Controls.Add(this.btnHashSet);
             this.Controls.Add(this.btnList);
             this.Controls.Add(this.lista);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Listas Genericas";
@@ -157,6 +170,7 @@
         private System.Windows.Forms.Button btnSortedDict;
         private System.Windows.Forms.Button btnSortedSet;
         private System.Windows.Forms.Button btnQueue;
+        private System.Windows.Forms.Button btnStack;
     }
 }
 
