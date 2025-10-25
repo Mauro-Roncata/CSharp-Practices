f23751ff6bc2e3a72f113348d6b60e4a2d9ff396
Mauro-Roncata <mauroroncata@gmail.com>
2025-10-25 17:46:59 -0300

    class MessageBox pratice

---
+++ ImportantClass/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ImportantClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncMsgBox = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncMsgBox
+            // 
+            this.btncMsgBox.Location = new System.Drawing.Point(12, 357);
+            this.btncMsgBox.Name = "btncMsgBox";
+            this.btncMsgBox.Size = new System.Drawing.Size(197, 81);
+            this.btncMsgBox.TabIndex = 0;
+            this.btncMsgBox.Text = "MessageBox";
+            this.btncMsgBox.UseVisualStyleBackColor = true;
+            this.btncMsgBox.Click += new System.EventHandler(this.btncMsgBox_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(12, 18);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(162, 73);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btncMsgBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncMsgBox;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ ImportantClass/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImportantClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncMsgBox_Click(object sender, EventArgs e)
+        {
+            /*MessageBox.Show("Amo viver","como eu amo essa vida");
+            DialogResult res = MessageBox.Show("Texto da mensagem", "Titulo", MessageBoxButtons.YesNo);
+
+            if (res == DialogResult.Yes)
+            {
+                lblResult.Text = "SIM clicado";
+                    }
+            else if (res == DialogResult.No) 
+            {
+                lblResult.Text = "NO clicked";
+            }*/
+
+            MessageBox.Show("Mensagem","Titulo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+            
+
+        }
+    }
+}
