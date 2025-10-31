4242e5b7e2dd4191554cc820846a570db1dbef9c
Mauro-Roncata <mauroroncata@gmail.com>
2025-10-31 10:35:38 -0300

    class Task pratices

---
+++ TaskConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           /* Task t1 = new Task(Tarefa);
+            t1.Start();
+
+            Task t2 =  Task.Run(Tarefa);
+
+            Task.Run(Tarefa);*/
+
+            Task.Factory.StartNew(Tarefa);
+
+            Task.Run(() => 
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Tarefa do anônima");
+                }
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Principal");
+            }
+        }
+
+        static private void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa do TASK");
+            }
+        }
+    }
+}
