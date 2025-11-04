bb4253928c8fdca47b67d6f3887e26acba8ae6db
Mauro-Roncata <mauroroncata@gmail.com>
2025-11-04 11:16:12 -0300

    Form with Threads exercise

--- Formulario/FormSecond.Designer.cs
+++ Formulario/FormSecond.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.btnPrincip = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(43, 36);
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(272, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Second Form";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // btnPrincip
+            // 
+            this.btnPrincip.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrincip.Location = new System.Drawing.Point(281, 284);
+            this.btnPrincip.Name = "btnPrincip";
+            this.btnPrincip.Size = new System.Drawing.Size(221, 76);
+            this.btnPrincip.TabIndex = 1;
+            this.btnPrincip.Text = "Principal";
+            this.btnPrincip.UseVisualStyleBackColor = true;
+            this.btnPrincip.Click += new System.EventHandler(this.btnPrincip_Click);
+            // 
             // FormSecond
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrincip);
             this.Controls.Add(this.label1);
             this.Name = "FormSecond";
             this.Text = "FormSecond";
@@ -58,5 +71,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPrincip;
     }
 }
