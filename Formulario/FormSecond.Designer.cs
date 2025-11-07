f915b0c1d4a91fd034d0eece8484f1637ba235fc
Mauro-Roncata <mauroroncata@gmail.com>
2025-11-07 10:59:54 -0300

    WinForm menuBar actions

--- Formulario/FormSecond.Designer.cs
+++ Formulario/FormSecond.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btnPrincip = new System.Windows.Forms.Button();
+            this.txtMens = new System.Windows.Forms.TextBox();
+            this.btnRetorno = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +49,7 @@
             // btnPrincip
             // 
             this.btnPrincip.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPrincip.Location = new System.Drawing.Point(281, 284);
+            this.btnPrincip.Location = new System.Drawing.Point(567, 362);
             this.btnPrincip.Name = "btnPrincip";
             this.btnPrincip.Size = new System.Drawing.Size(221, 76);
             this.btnPrincip.TabIndex = 1;
@@ -54,15 +57,49 @@
             this.btnPrincip.UseVisualStyleBackColor = true;
             this.btnPrincip.Click += new System.EventHandler(this.btnPrincip_Click);
             // 
+            // txtMens
+            // 
+            this.txtMens.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMens.Location = new System.Drawing.Point(190, 213);
+            this.txtMens.Name = "txtMens";
+            this.txtMens.Size = new System.Drawing.Size(429, 53);
+            this.txtMens.TabIndex = 2;
+            // 
+            // btnRetorno
+            // 
+            this.btnRetorno.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetorno.Location = new System.Drawing.Point(340, 362);
+            this.btnRetorno.Name = "btnRetorno";
+            this.btnRetorno.Size = new System.Drawing.Size(221, 76);
+            this.btnRetorno.TabIndex = 3;
+            this.btnRetorno.Text = "Retorno";
+            this.btnRetorno.UseVisualStyleBackColor = true;
+            this.btnRetorno.Click += new System.EventHandler(this.btnRetorno_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(113, 362);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(221, 76);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
             // FormSecond
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnRetorno);
+            this.Controls.Add(this.txtMens);
             this.Controls.Add(this.btnPrincip);
             this.Controls.Add(this.label1);
             this.Name = "FormSecond";
             this.Text = "FormSecond";
+            this.Load += new System.EventHandler(this.FormSecond_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,5 +109,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnPrincip;
+        private System.Windows.Forms.TextBox txtMens;
+        private System.Windows.Forms.Button btnRetorno;
+        private System.Windows.Forms.Button btnFechar;
     }
 }
