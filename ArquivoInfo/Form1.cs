47e252ea6d05d8013c5d1387fa66b8b8809f6bd7
Mauro-Roncata <mauroroncata@gmail.com>
2025-11-12 15:21:48 -0300

    FileInfo exercise

---
+++ ArquivoInfo/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArquivoInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileInfo info = new FileInfo("C:\\Users\\Usuario\\source\\repos\\teste.txt");
+
+            lista.Items.Clear();
+            lista.Items.Add(info.Name);
+            lista.Items.Add(info.FullName);
+            lista.Items.Add(info.Extension);
+            lista.Items.Add(info.Directory);
+            lista.Items.Add(info.DirectoryName);
+            lista.Items.Add(info.CreationTime);
+
+        }
+    }
+}
